193c6fda470805d48cb555eb099f396bfe13a80b
Rupesh Bhochhibhoya <bhochhi@gmail.com>
2018-04-09 23:42:15 -0500

    new my accounts

--- examples/spring-sm/src/main/java/com/codewithme/bhochhi/sm/Application.java
+++ examples/spring-sm/src/main/java/com/codewithme/bhochhi/sm/Application.java
@@ -6,31 +6,30 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.statemachine.StateMachine;
 
-
 @SpringBootApplication
 public class Application implements CommandLineRunner {
-	
+
 	@Autowired
 	private StateMachine<States, Events> stateMachine;
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+//		SpringApplication app = new SpringApplication(Application.class);
+//		app.setWebEnvironment(false); // <<<<<<<<<
+//		app.run(args);
+		 SpringApplication.run(Application.class, args);
+	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		stateMachine.sendEvent(Events.E1);
+		stateMachine.sendEvent(Events.E2);
+	}
 
-    @Override
-    public void run(String... args) throws Exception {
-        stateMachine.sendEvent(Events.E1);
-        stateMachine.sendEvent(Events.E2);
-    }
-	
 	public enum States {
-	    SI, S1, S2
+		SI, S1, S2
 	}
 
 	public enum Events {
-	    E1, E2
+		E1, E2
 	}
 }
-
-
