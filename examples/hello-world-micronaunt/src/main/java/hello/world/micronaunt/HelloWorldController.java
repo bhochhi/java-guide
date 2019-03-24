8366b9b2017bd6f26fd044162843b28d05db7556
Rupesh Bhochhibhoya <bhochhi@gmail.com>
2019-03-24 11:22:32 -0500

    playing with swagger

--- examples/hello-world-micronaunt/src/main/java/hello/world/micronaunt/HelloWorldController.java
+++ examples/hello-world-micronaunt/src/main/java/hello/world/micronaunt/HelloWorldController.java
@@ -1,10 +1,16 @@
 package hello.world.micronaunt;
 
 
+import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.validation.Validated;
 import io.reactivex.Single;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import javax.validation.constraints.NotBlank;
 
@@ -13,7 +19,21 @@ import javax.validation.constraints.NotBlank;
 public class HelloWorldController {
 
 
-    @Get("/hello/{name}")
+    /**
+     * @param name The person's name
+     * @return The greeting
+     */
+    @Get(uri="/hello/{name}", produces= MediaType.TEXT_PLAIN)
+    @Operation(summary = "Greets a person",
+            description = "A friendly greeting is returned"
+    )
+    @ApiResponse(
+            content = @Content(mediaType = "text/plain",
+                    schema = @Schema(type="string"))
+    )
+    @ApiResponse(responseCode = "400", description = "Invalid Name Supplied")
+    @ApiResponse(responseCode = "404", description = "Person not found")
+    @Tag(name = "greeting")
     public Single<String> hello(@NotBlank String name){
         return Single.just("Hello "+name+"!!");
     }
